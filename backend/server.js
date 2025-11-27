d8f25a99474d3037a1ac72993775b95ac656ed94
Anthony Lopez <lopezanthony1671@gmail.com>
2025-11-27 02:57:05 -0800

    - Built auth routes (/auth/signup, /auth/login) with Zod validation - Connected backend to frontend via fetch POST requests - Added input validation on signup/login pages (regex, error messages) - Configured Express server with static file hosting - Added routes for /, /login, /signup and fixed path issues - Organized frontend folder structure and removed unused index.html - Verified API using Thunder Client and tested successful user sign-up/login

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,38 @@
 import express from "express";
 import authRoute from "./auth/authRoute.js";
 
-const app = express();
+import path from "path";
+import { fileURLToPath } from "url";
+import { _date } from "zod/v4/core";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
 app.use(express.json());
+
+// Serve static frontend files (HTML, CSS, JS)
+app.use(express.static(path.join(__dirname, "../frontend")));
+
 app.use("/auth", authRoute);
 
 app.get("/health", (_req, res) => res.json({ ok: true }));
+
+// Default route → homepage
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/Main/home.html"));
+});
+
+app.get("/login", (req, res) => {
+  res.sendFile(
+    path.join(__dirname, "../frontend/credentialsPage/loginPage.html")
+  );
+});
+
+app.get("/signup", (req, res) => {
+  res.sendFile(
+    path.join(__dirname, "../frontend/credentialsPage/signupPage.html")
+  );
+});
 
 app.listen(3000, () => console.log("API on http://localhost:3000"));
